85d08af664182d2ec19fa1fea835e527d074f8b8
Kaeri <kathleenepovadora@gmail.com>
2025-10-08 14:51:12 +0300

    Refactor ball direction logic and update killzone handling
    Refactored ball.gd to modularize and clarify random direction generation for the ball's movement. Updated killzone.gd to remove time scaling effects and improve debug output when a ball passes the killzone.

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,19 +1,50 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
+
 var direction = Vector2.ZERO
 
 func _ready() -> void:
 	# Start the ball in a random horizontal direction
-	var x_dir = [-1, 1].pick_random()
-	var y_dir = randf_range(-0.5, 0.5)
-	direction = Vector2(x_dir, y_dir).normalized()
+	# With random vertical rotation
+	direction = get_random_direction()
 	velocity = direction * SPEED
 
-func _physics_process(_delta: float) -> void:
-	var collision = move_and_collide(velocity * _delta)
+func _physics_process(delta: float) -> void:
+	var collision = move_and_collide(velocity * delta)
 	
 	if collision:
 		# Reflect the velocity vector when hitting something
 		var normal = collision.get_normal()
 		velocity = velocity.bounce(normal)
+
+
+# Get random horizontal direction
+func get_random_x() -> int:
+	# Pick random direction, either Left or Right
+	# Multiplication to +1 does not change the value
+	# But multiplication to -1 does change it's direction
+	return [-1, 1].pick_random()
+
+# Get random vertical rotation
+func get_random_y() -> float:
+	# Get random direction, Up, Down or anything in-between
+	# It's float value, so it can be 0.34 or -0.123
+	# It decides how much random rotation it will have
+	# We need to make the range between PI value
+	return randf_range(-2, 2)
+
+func get_direction_vector(x_dir: int, y_dir: float) -> Vector2:
+	# We create a new vector (2d) from x and y values and we normalize it
+	# Normalizing is the process when the vector magnitude is scaled to 1
+	# While the direction/angle is preserved
+	#
+	# In our case, it's to make sure it won't change the ball speed
+	# We only need its direction
+	return Vector2(x_dir, y_dir).normalized()
+
+func get_random_direction() -> Vector2:
+	var x_dir = get_random_x()
+	var y_dir = get_random_y()
+	
+	return get_direction_vector(x_dir, y_dir)
--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -3,11 +3,11 @@ extends Area2D
 @onready var timer: Timer = $Timer
 
 func _on_body_entered(body: Node) -> void:
-	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
+
+	print("Ball passed the line", body)
+
 	timer.start()
-	print(body)
 	
 func _on_timer_timeout() -> void:
-	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
