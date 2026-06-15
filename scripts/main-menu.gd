59c5fa964173358dfd366d07fa7f1cf7f07f31ee
Kathleen Povadora <60345682+kaeri-gg@users.noreply.github.com>
2026-06-15 22:10:34 +0300

    Revamp main menu and viewport

--- scripts/main-menu.gd
+++ scripts/main-menu.gd
@@ -1,13 +1,7 @@
 extends Control
 
 # Buttons
-@onready var start_game_button: Button = $StartGameButton
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	start_game_button.pressed.connect(_on_start_game_button_pressed)
-
+@onready var start_game_button: Button = %StartGameButton
 
 func _on_start_game_button_pressed() -> void:
 	# load game scene
